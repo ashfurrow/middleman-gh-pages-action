2fcc2f88048dab1a6e7a880cc75923cdc62cb412
Ash Furrow <ash@ashfurrow.com>
2024-10-10 22:10:53 -0300

    Fixes git permission issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ bundle list | grep "middleman ("
 
 echo 'Publishing site...'
 
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+
 # We use this instead: https://github.com/edgecase/middleman-gh-pages
 bundle exec rake yaml:sort
 bundle exec publish ALLOW_DIRTY=true
